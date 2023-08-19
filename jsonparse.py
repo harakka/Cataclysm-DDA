22d647e344ba55f833611f16f7c899dc80e10b39
Antti Riikonen <harava@gmail.com>
2023-08-19 08:28:09 +0000

    produce csv file, skip timing info since code is fast enough now

--- jsonparse.py
+++ jsonparse.py
@@ -7,60 +7,65 @@ from time import sleep
 from timeit import default_timer
 
 
-def isObjectDefinition(item): 
+def isObjectDefinition(item):
     if isinstance(item, dict) and "id" in item and "type" in item:
         return True
     return False
 
+
 def without_keys(d, keys):
     return {x: d[x] for x in d if x not in keys}
 
+
 def getValuesRecursively(item):
-    result = []
+    result = set()
     if isinstance(item, dict):
         if isObjectDefinition(item):
             # this is a type definition
-            exclude_keys = {'exclude', 'exclude2'}
+            exclude_keys = {"id", "type"}
             item = without_keys(item, exclude_keys)
         for key in item:
             # if key != "id" and key != "type":
             #     if "type" not in item:
             #         print(f"key was {key}")
-            result.extend(getValuesRecursively(item[key]))
+            result.update(getValuesRecursively(item[key]))
             # if key "id" and "type" in item:
     elif isinstance(item, list):
         for listitem in item:
-            result.extend(getValuesRecursively(listitem))
+            result.update(getValuesRecursively(listitem))
     else:
-        result.append(item)
+        result.add(item)
     return result
 
-# dataDirs = ["core","json", "mods"]
-dataDirs = ["core","json"]
+
+dataDirs = ["core", "json", "mods"]
+# dataDirs = ["core", "json"]
 argparser = ArgumentParser()
-argparser.add_argument("jsondir", help="Directory that contains the json files you want to analyze")
+argparser.add_argument(
+    "jsondir", help="Directory that contains the json files you want to analyze"
+)
+# argparser.add_argument("dest", help="File to write the results into.")
 args = argparser.parse_args()
-jsonFiles = []
+jsonFiles = set()
 if not isdir(args.jsondir):
     print("Error: " + args.jsondir + " is not a folder")
     exit(1)
 
 basedir = Path(args.jsondir)
 
-print(str(dataDirs))
+time_start = default_timer()
+for subDir in dataDirs:
+    for path, _, files in walk(Path(join(args.jsondir, subDir))):
+        for sourceFile in files:
+            fullPath = join(path, sourceFile)
+            if fullPath.endswith(".json") and "obsolet" not in fullPath:
+                jsonFiles.add(fullPath)
+# print(
+#     f"Find files: {default_timer() - time_start}s, {len(jsonFiles)} json source files"
+# )
 
 time_start = default_timer()
-for subDir in dataDirs: 
-    for p, d, f in walk(Path(join(args.jsondir, subDir))):
-            for sourceFile in f:
-                if sourceFile.endswith('.json'):
-                    jsonFiles.append(join(p, sourceFile))
-
-print(f"Find files: {default_timer() - time_start}s, {len(jsonFiles)} json source files")
-
-
-time_start = default_timer()
-jsonIds = []
+jsonIds = set()
 for jsonFile in jsonFiles:
     with open(jsonFile) as jsonDataSource:
         data = json.load(jsonDataSource)
@@ -73,30 +78,30 @@ for jsonFile in jsonFiles:
                 itemType = item["type"]
                 if isinstance(item["id"], list):
                     for subItem in item["id"]:
-                        jsonIds.append((subItem, itemType, jsonFile))
+                        jsonIds.add((subItem, itemType, jsonFile))
                 else:
-                    jsonIds.append((item["id"], itemType, jsonFile))
-print(f"Find ids: {default_timer() - time_start}s, {len(jsonIds)} ids")
+                    jsonIds.add((item["id"], itemType, jsonFile))
+# print(f"Find ids: {default_timer() - time_start}s, {len(jsonIds)} ids")
 
 time_start = default_timer()
-jsonValues = []
+jsonValues = set()
 for jsonFile in jsonFiles:
     with open(jsonFile) as jsonDataSource:
         data = json.load(jsonDataSource)
         flatData = getValuesRecursively(data)
-        # print("flattened " + jsonFile + ", " + str(len(flatData)) + " entries")
-        jsonValues = jsonValues + flatData
-print(f"Find values: {default_timer() - time_start}s, {len(jsonValues)} values")
+        # print(f"flattened {jsonFile}, {flatData} entries. Data: {flatData}")
+        jsonValues.update(flatData)
+# print(f"Find values: {default_timer() - time_start}s, {len(jsonValues)} values")
 
-# time_start = default_timer()
-# i = 0
-# idsWithoutMatch = []
-# for jsonId, jsonType, sourceFile in jsonIds:
-#     # print(str(i) + "/" + str(len(jsonIds)) + " " + jsonId + " in " + sourceFile)
-
-#     if jsonId not in jsonValues:
-#         idsWithoutMatch.append((jsonId, jsonType, sourceFile))
-#     i += 1
-# # for id, type, file in idsWithoutMatch:
-# #     print(id + ";" + type + ";" + file)
+print("id;type;file")
+time_start = default_timer()
+i = 0
+idsWithoutMatch = set()
+for jsonId, jsonType, sourceFile in jsonIds:
+    # print(f"Finding match for {jsonId}/{jsonType} ({sourceFile})")
+    if jsonId not in jsonValues:
+        idsWithoutMatch.add((jsonId, jsonType, sourceFile))
+    i += 1
+for id, type, file in idsWithoutMatch:
+    print(id + ";" + type + ";" + file)
 # print(f"Crossreference: {default_timer() - time_start}s")
